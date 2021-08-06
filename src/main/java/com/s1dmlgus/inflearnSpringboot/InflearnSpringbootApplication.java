216b8b149dd621b4fc77faa36ab204427b915cae
t1dmlgus <dmlgusgngl@gmail.com>
2021-08-06 17:41:14 +0900

    Initial commit

---
+++ src/main/java/com/s1dmlgus/inflearnSpringboot/InflearnSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.inflearnSpringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/s1dmlgus/inflearnSpringboot/InflearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1dmlgus.inflearnSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
