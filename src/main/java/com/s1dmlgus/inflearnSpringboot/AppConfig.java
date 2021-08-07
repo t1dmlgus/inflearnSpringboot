71095c85bbf8a639f1f531cdce0d8281c5fe1cc0
t1dmlgus <dmlgusgngl@gmail.com>
2021-08-07 11:54:48 +0900

    refactor: AppConfig 리펙토링
    AppConfig에서 설정한 클래스의 역할이 드러날 수 있도록 리펙토링
    
    - memberServie 역할
    -
    memberRepository 역할
    - orderService 역할
    - discountPolicy 역할(할인 정책)

--- src/main/java/com/s1dmlgus/inflearnSpringboot/AppConfig.java
+++ src/main/java/com/s1dmlgus/inflearnSpringboot/AppConfig.java
@@ -1,5 +1,6 @@
 package com.s1dmlgus.inflearnSpringboot;
 
+import com.s1dmlgus.inflearnSpringboot.domain.discount.DiscountPolicy;
 import com.s1dmlgus.inflearnSpringboot.domain.discount.FixDiscountPolicy;
 import com.s1dmlgus.inflearnSpringboot.domain.member.MemoryMemberRepository;
 import com.s1dmlgus.inflearnSpringboot.domain.order.OrderService;
@@ -11,12 +12,25 @@ import com.s1dmlgus.inflearnSpringboot.service.member.MemberServiceImpl;
 // 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스
 public class AppConfig {
 
+
+    // memeberService 역할
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    // memeberRepository 역할
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // orderService 역할
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    // 할인 정책 역할
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 
 
