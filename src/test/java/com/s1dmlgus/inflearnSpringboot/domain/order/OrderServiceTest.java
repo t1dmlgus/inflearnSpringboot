ea9d174cd9da8d148109061e8bf29cdecc71b09e
t1dmlgus <dmlgusgngl@gmail.com>
2021-08-07 09:29:28 +0900

    test: 주문 생성 테스트

---
+++ src/test/java/com/s1dmlgus/inflearnSpringboot/domain/order/OrderServiceTest.java
@@ -0,0 +1,33 @@
+package com.s1dmlgus.inflearnSpringboot.domain.order;
+
+import com.s1dmlgus.inflearnSpringboot.domain.member.Grade;
+import com.s1dmlgus.inflearnSpringboot.domain.member.Member;
+import com.s1dmlgus.inflearnSpringboot.service.member.MemberService;
+import com.s1dmlgus.inflearnSpringboot.service.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    // 주문 테스트
+    @Test
+    public void createOrderTest() throws Exception{
+        //given
+        long memberId = 1L;
+        Member memberA = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        //when
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        //then
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+
+}
